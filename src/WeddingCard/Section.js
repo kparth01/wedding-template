a146c25028ab1e824fe771f7cb7d7f0321475bbc
Parth Kansara <kparth01@gmail.com>
2022-08-24 02:44:05 +0800

    Cleanup.

--- src/WeddingCard/Section.js
+++ src/WeddingCard/Section.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default function Section({ title, subtitle, dark, id }) {
+export default function Section({ title, content, when, where, dark, id }) {
 	return (
 		<div className={"section" + (dark ? " section-dark" : "")}>
 			<div className="section-content" id={id}>
 				<h1>{title}</h1>
-				<p>{subtitle}</p>
+				<p>{content}</p>
 			</div>
 		</div>
 	);
